c9b403d7cc96019d988e60393f96940e154d8cba
Simon Cheng <simoncheng559@gmail.com>
2021-01-09 15:32:06 -0500

    add more informational errors

--- test/util/readfile.cli.js
+++ test/util/readfile.cli.js
@@ -9,12 +9,12 @@ let cacheFile = path.join(fixtures.validNginxCache, fixtures.testCacheFile);
 module.exports.cliArguments = ['readfile', cacheFile];
 module.exports.validateResponse = function validateResponse(response){
     if(response.exitCode){
-        throw 'unexpected exit code ' + exitCode;
+        throw 'unexpected exit code ' + response.exitCode + '. response: ' + JSON.stringify(response);
     }
     if(Array.from(response.stdout.matchAll(/Key: http:\/\/127.0.0.1:1935/g)).length !== 1){
-        throw 'expected there to be 1 occurrence of "Key: http://127.0.0.1:1935" but stdout is ' + response.stdout;
+        throw 'expected stdout to contain 1 occurrence of "Key: http://127.0.0.1:1935". response: ' + JSON.stringify(response);
     }
     if(response.stderr){
-        throw 'expected stderr to contain nothing, instead, contains ' + response.stderr;
+        throw 'expected stderr to contain nothing. response:  ' + JSON.stringify(response);
     }
 };
